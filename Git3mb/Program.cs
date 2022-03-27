f282f64897d4ae0f515192551642616374466b38
Proxeyes <radinom2@mail.ru>
2022-03-27 23:11:23 +0500

    sloj

--- Git3mb/Program.cs
+++ Git3mb/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Выберите знак");
+Console.WriteLine("1. +");
+Console.WriteLine("2. -");
+Console.WriteLine("3. :");
+Console.WriteLine("4. *");
+c = Convert.ToInt32(Console.ReadLine());
+if (c == 1)
+{
+    Console.WriteLine(a + b);
 
+}
